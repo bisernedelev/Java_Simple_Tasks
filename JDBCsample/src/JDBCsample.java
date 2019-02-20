085969770a0050b3bc67906d8e3fb84b87c64fb9
bibo <bibo0088@abv.bg>
2019-02-20 16:00:30 +0200

    JDBC Example

---
+++ JDBCsample/src/JDBCsample.java
@@ -0,0 +1,26 @@
+import java.sql.*;
+
+public class JDBCsample {
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://127.0.0.1:3306/trymimi";
+        String user = "root";
+        String password = "18991948";
+
+        try (Connection conn = DriverManager.getConnection(url, user, password);
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery("select * from mimi")) {
+
+            while (rs.next()) {
+
+                System.out.printf("%d %s %d%n",
+                        rs.getInt("id"), rs.getString("name"),
+                        //  rs.getString("type")
+                        rs.getInt("age"));
+
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
